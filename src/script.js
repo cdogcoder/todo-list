89430ae6468631b080f535493121dc09475ba091
cdogcoder <cdogcoder@gmail.com>
2025-05-14 06:30:17 -0700

    Intermediate commit

--- src/script.js
+++ src/script.js
@@ -100,8 +100,36 @@ function deleteTodoItem(itemTitle) {
     displayProjectAndItems(currentProjectTitle, currentProjectDescription, currentProjectTodoItems);
 }
 
+function editTodoItem(itemTitle, itemDescription, itemDueDate, itemPriority) {
+    const currentProjectTitle = document.querySelector(".project-title").textContent;
+    const currentProjectDescription = projects[currentProjectTitle][0];
+    let currentProjectTodoItems = projects[currentProjectTitle].slice(1);
+    const selectedProjectTodoItemIndex = currentProjectTodoItems.findIndex(item => itemTitle == item["title"]);
+    currentProjectTodoItems[selectedProjectTodoItemIndex] = {
+        title: itemTitle,
+        description: itemDescription,
+        dueDate: itemDueDate,
+        priority: itemPriority
+    }
+    projects[currentProjectTitle] = [currentProjectDescription, ...currentProjectTodoItems];
+    displayProjectAndItems(currentProjectTitle, currentProjectDescription, currentProjectTodoItems);
+}
+
+function saveItemEditChanges() {
+    const editTodoItemTitleInput = document.querySelector("#edit-todo-item-title");
+    const editTodoItemDescriptionInput = document.querySelector("#edit-todo-item-description");
+    const editTodoItemDueDateInput = document.querySelector("#edit-todo-item-due-date");
+    const editTodoItemPriorityInput = document.querySelector("#edit-todo-item-priority");
+    editTodoItem(editTodoItemTitleInput.value, editTodoItemDescriptionInput.value, editTodoItemDueDateInput.value, editTodoItemPriorityInput.value);
+    editTodoItemTitleInput.value = "";
+    editTodoItemDescriptionInput.value = "";
+    editTodoItemDueDateInput.value = "";
+    editTodoItemPriorityInput.value = "";
+}
+
 function createAndAppendDOMItemElements(item) {
     const mainWindow = document.querySelector(".main-window");
+    const editTodoItemDialog = document.querySelector(".edit-todo-item-dialog");
     const formattedItemDate = [...item["dueDate"].split("-").slice(1), item["dueDate"].split("-")[0]].join("/");
     const todoItem = document.createElement("div");
     todoItem.classList = "todo-item";
@@ -126,6 +154,7 @@ function createAndAppendDOMItemElements(item) {
     const editTodoItemButton = document.createElement("button");
     editTodoItemButton.classList = "edit-todo-item-button";
     editTodoItemButton.textContent = "Edit";
+    editTodoItemButton.addEventListener("click", () => editTodoItemDialog.showModal());
     const deleteTodoItemButton = document.createElement("button");
     deleteTodoItemButton.classList = "delete-todo-item-button";
     deleteTodoItemButton.textContent = "Delete";
@@ -237,8 +266,5 @@ addTodoItemButton.addEventListener("click", () => {
 const saveNewTodoItemButton = document.querySelector(".save-new-todo-item-button");
 saveNewTodoItemButton.addEventListener("click", saveNewTodoItem);
 
-const editTodoItemDialog = document.querySelector(".edit-todo-item-dialog");
-const editTodoItemButton = document.querySelector(".edit-todo-item-button");
-editTodoItemButton.addEventListener("click", () => {
-    editTodoItemDialog.showModal();
-})
+const saveEditTodoItemButton = document.querySelector(".save-edit-todo-item-button");
+saveEditTodoItemButton.addEventListener("click", saveItemEditChanges);
